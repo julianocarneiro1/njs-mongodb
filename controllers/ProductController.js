d140b5473b65e37f86fa9e9af39b0d02ec2e0887
Juliano <julianoomc@gmail.com>
2023-02-23 16:56:32 -0300

    Inserindo dados no banco

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,7 +1,24 @@
 const Product = require('../models/Product')
 
 module.exports = class ProductController {
+
     static showProducts(req, res) {
         res.render('products/all')
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static createProductPost(req, res) {
+        const product = new Product(
+            req.body.name,
+            req.body.price,
+            req.body.description
+        )
+
+        product.save()
+
+        res.redirect('/products')
     }
 }
